6c399f1d7916377103696e9bc89c042e205fffa0
ddycherry-hue <ddy.cherry@gmail.com>
2026-07-08 19:32:12 +0600

    Add files via upload
    spring,number and boolean

---
+++ HW_1/script.js
@@ -0,0 +1,24 @@
+let myName = "John Doe";
+let mySurname = "Smith";
+let myCity = "New York";
+let myHeight = 180;
+let myWeight = 95;
+let myAge = 18;
+
+true
+false
+
+let userName = prompt("Your name?");
+Text
+let userSurname = prompt("Your surname?");
+let userCity = prompt("Your city?");
+let userAge = prompt("Your age?");
+
+console.log = confirm("Are you sure?");
+
+let Gender = true
+if (Gender== true) {
+    console.log("he is sure");
+} else {
+    console.log("he is not sure");
+}
